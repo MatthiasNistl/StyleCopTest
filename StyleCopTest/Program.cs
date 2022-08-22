17f911dbde55084a67a03badbbe2216a43a6f19a
Matthias Nistl <matthias.nistl@gmx.de>
2022-08-22 20:00:12 +0200

    Initial commit

---
+++ StyleCopTest/Program.cs
@@ -0,0 +1,13 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        #region MyRegion
+
+        const string dummy = "Hello, World!";
+
+        #endregion
+
+        Console.WriteLine(dummy);
+    }
+}
